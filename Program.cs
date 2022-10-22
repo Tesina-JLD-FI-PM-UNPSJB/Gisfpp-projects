7c5b61346d769fd79dfcd6b7d91e683679cd6701
jldevia <jldevia81@gmail.com>
2022-10-22 09:57:48 -0300

    Se agrega habilitación de CORS

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -30,6 +29,16 @@ builder.Services.AddDbContext<ProjectDbContext>(
                 //.EnableDetailedErrors()
 );
 
+builder.Services.AddCors( options => {
+    options.AddDefaultPolicy(
+        policy => {
+            policy.WithOrigins("http://localhost:4200", "http://localhost:8080")
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        }
+    );
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -37,7 +46,8 @@ app.UseExceptionHandler("/errors");
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseCors();
+//app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
